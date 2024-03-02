108ecc493b3cd171251dc22b4bdc022f75c13258
John Webb <john.webb94@gmail.com>
2024-03-02 12:26:16 -0700

    Setup learn elixir

---
+++ test/learn_elixir/bucket_test.exs
@@ -0,0 +1,16 @@
+defmodule LearnElixir.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = LearnElixir.Bucket.start_link([])
+    %{bucket: bucket}
+
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert LearnElixir.Bucket.get(bucket, "milk") == nil
+
+    LearnElixir.Bucket.put(bucket, "milk", 3)
+    assert LearnElixir.Bucket.get(bucket, "milk") == 3
+  end
+end
